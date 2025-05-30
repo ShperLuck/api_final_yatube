289b2715befd707d4459902559b64d13c409d989
ShperLuck <nikitaboickov4@gmail.com>
2025-05-30 11:58:27 +0300

    Update serializers.py

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -3,49 +3,38 @@ from rest_framework.relations import SlugRelatedField
 from posts.models import Comment, Post, Group, Follow, User
 
 # Сериализатор для модели Post
-
-
 class PostSerializer(serializers.ModelSerializer):
-    # Поле author отображает имя пользователя, только для чтения
     author = SlugRelatedField(slug_field='username', read_only=True)
 
     class Meta:
-        fields = '__all__'
-        required = ('text',)
+        fields = ('id', 'text', 'author', 'image', 'group', 'pub_date')
         model = Post
+        read_only_fields = ('id', 'author', 'pub_date')
+        required_fields = ('text',)
 
 # Сериализатор для модели Comment
-
-
 class CommentSerializer(serializers.ModelSerializer):
-
     author = serializers.SlugRelatedField(
         read_only=True, slug_field='username'
     )
 
     class Meta:
-        fields = '__all__'
-        required = ('text',)
-        read_only_fields = ('author', 'post')
+        fields = ('id', 'author', 'post', 'text', 'created')
         model = Comment
+        read_only_fields = ('id', 'author', 'post', 'created')
+        required_fields = ('text',)
 
 # Сериализатор для модели Group
-
-
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
-        fields = '__all__'
+        fields = ('id', 'title', 'slug', 'description')
         model = Group
 
 # Сериализатор для модели Follow
-
-
 class FollowSerializer(serializers.ModelSerializer):
-    # Поле user отображает имя пользователя, только для чтения
     user = serializers.SlugRelatedField(
         read_only=True, slug_field='username'
     )
-    # Поле following позволяет выбрать пользователя для подписки
     following = serializers.SlugRelatedField(
         slug_field='username',
         queryset=User.objects.all()
@@ -53,18 +42,15 @@ class FollowSerializer(serializers.ModelSerializer):
 
     class Meta:
         fields = ('user', 'following')
-        read_only_fields = ('user',)
-        required = ('user', 'following')
         model = Follow
+        read_only_fields = ('user',)
+        required_fields = ('following',)
 
-    # Валидация данных
     def validate(self, data):
         user = self.context['request'].user
         following = data['following']
-        # Проверяет, что пользователь не подписывается на себя
         if user == following:
             raise serializers.ValidationError('You can\'t follow yourself.')
-        # Проверяет, что подписка еще не существует
         if Follow.objects.filter(user=user, following=following).exists():
             raise serializers.ValidationError(
                 'You\'re already following this user.'
