6bb49faba82544306e6daef34faff9700456e870
ShperLuck <nikitaboickov4@gmail.com>
2025-05-30 11:53:27 +0300

    Update urls.py

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,23 +1,25 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import (
-    PostViewSet,
-    CommentViewSet,
-    GroupViewSet,
-    FollowViewSet,
-)
+from rest_framework.authtoken.views import obtain_auth_token
+from .views import PostViewSet, CommentViewSet, GroupViewSet, FollowViewSet
 
-# Инициализация роутера
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='posts')
 router.register(r'groups', GroupViewSet, basename='groups')
 router.register(r'follow', FollowViewSet, basename='follow')
-router.register(r'posts/(?P<post_pk>\d+)/comments', CommentViewSet, basename='comments')
 
-# URL-шаблоны
 urlpatterns = [
-    # Маршруты роутера
-    path('v1/', include(router.urls)),
-    # JWT-эндпоинты через Djoser
-    path('v1/auth/', include('djoser.urls.jwt')),  # /jwt/create/, /jwt/refresh/, /jwt/verify/
+    path('v1/', include(router.urls)),  # Маршруты для ViewSet'ов
+    path('v1/api-token-auth/', obtain_auth_token, name='api-token-auth'),  # Эндпоинт для получения токена
+    path('v1/', include('djoser.urls')),  # Дополнительные маршруты  djoser
+    path('v1/posts/<int:post_pk>/comments/', CommentViewSet.as_view({
+        'get': 'list',
+        'post': 'create',
+    }), name='comment-list'),
+    path('v1/posts/<int:post_pk>/comments/<int:pk>/', CommentViewSet.as_view({
+        'get': 'retrieve',
+        'put': 'update',
+        'patch': 'partial_update',
+        'delete': 'destroy',
+    }), name='comment-detail'),
 ]
